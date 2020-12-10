fixed denoise scaling + new ae model

denoising.py
@@ -5,7 +5,7 @@ import math
 from tensorflow.keras.constraints import max_norm
 from tensorflow.keras.layers import Conv2D, Conv2DTranspose
 from tensorflow.keras.models import Sequential
-import tensorflow
+import tensorflow as tf
 
 
 def add_noise(s, noise_factor = 0.1):
@@ -16,9 +16,11 @@ def add_noise(s, noise_factor = 0.1):
 
 def reshape_signal(signal, width=8, height=5):
     s = []
+    mine = np.amin(signal)
+    maxe = np.amax(signal)
     for i in range(0, len(signal)):
         sample = signal[i]
-        sample = (sample - np.min(sample)) / (np.max(sample) - np.min(sample))
+        sample = (sample - mine) / (maxe - mine)
         sample = sample.reshape(width, height)
         s.append(sample)
     s = np.array(s)
@@ -58,8 +60,6 @@ def train_model(pure_signal, noise_factor=0.1, width=8, height=5, epoch_cnt=10, 
     pure_r = reshape_signal(pure_signal, width, height)
     noisy_r = reshape_signal(noisy, width, height)
     
-    pure_train, pure_test = data_split(pure_r, 1)
-    noisy_train, noisy_test = data_split(noisy_r, 1)
     model = create_model(width,height)
     model.compile(optimizer='adam', loss='binary_crossentropy')
     model.fit(noisy_r, pure_r,
@@ -70,11 +70,17 @@ def train_model(pure_signal, noise_factor=0.1, width=8, height=5, epoch_cnt=10, 
     return model
 
 
-def denoise_signal(noisy_signals, model_fn="ae_denoise_model"):
+def denoise_signal(noisy_signals, model_fn="ae_denoise_model", model=None):
     signal = reshape_signal(noisy_signals)
-    model = tensorflow.keras.models.load_model(model_fn)
+    if model == None:
+        model = tf.keras.models.load_model(model_fn)
     cleared = model.predict(signal)
-    
     cleared = np.array(cleared).reshape((len(noisy_signals), 8 * 5,))
     return cleared
     
+    
+def denoise_dataset(ds: tf.data.Dataset, model_fn="ae_denoise_model"):
+    ds = list(ds.as_numpy_iterator())
+    model = tf.keras.models.load_model(model_fn)
+    cleared = denoise_signal(ds, model=model)
+    return tf.data.Dataset.from_tensor_slices(cleared)